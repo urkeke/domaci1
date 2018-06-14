a6cc7f4d44aa251cdd94663a8916de0b4f504114
Uros <urke.kekovic@gmail.com>
2018-06-14 02:54:10 +0200

    Button and first toast added.

--- app/src/main/java/adm/admtodoapp/MainActivity.java
+++ app/src/main/java/adm/admtodoapp/MainActivity.java
@@ -2,6 +2,12 @@ package adm.admtodoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.start_button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this,"First Toast Ever", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
